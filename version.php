965c292a457d784c63a93d1adb92cdcdfd64e75f
Owen Barritt <obarritt@wset.co.uk>
2015-07-15 10:09:38 +0100

    filter_cloudfront_signurl:  Add version.php

---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *  Cloudfront URL signing filter
+ *
+ *  This filter will replace defined cloudfront URLs with signed
+ *  URLs as described at http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-signed-urls.html
+ *
+ * @package    filter
+ * @subpackage cloudfront_signurl
+ * @copyright  2014 Owen Barritt, Wine & Spirit Education Trust
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+ defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2015071500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014051200;        // Requires Moodle 2.7
+$plugin->component = 'filter_cloudfront_signurl'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release   = '0.1 for Moodle 2.7+';
+?>
